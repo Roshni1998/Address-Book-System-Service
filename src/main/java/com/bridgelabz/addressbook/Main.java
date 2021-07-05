05a99ebae9f880bce553e457baebd3af182992e9
Roshni1998 <roshnimali847@gmail.com>
2021-07-05 19:10:22 +0530

    Display Welcome Message

---
+++ src/main/java/com/bridgelabz/addressbook/Main.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.addressbook;
+/*
+* @author ROSHNI
+* Address Book System Service
+* */
+
+public class Main {
+
+    // Main Method
+    public static void main(String[] args) {
+        System.out.println("*********************************************************");
+        System.out.println("**********Welcome to Address Book System Service*********");
+        System.out.println("*********************************************************");
+    }
+}
